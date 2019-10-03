b22fd69aef66b91d28c001a80e36ba30d487c32e
iguxa <tsybykov@bk.ru>
2019-10-04 00:16:27 +0300

    router 71 hash

--- vue-project/src/pages/Car.vue
+++ vue-project/src/pages/Car.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
         <h1>Car id {{id}}</h1>
+        <button class="btn btn-sm btn-default" @click="goBackToCars">Back</button>
+        <br>
+        <router-link
+                class="btn btn-info mt-2"
+                tag="button"
+                :to="{
+                    name:'carFull',
+                    params:{id},
+                    query:{
+                        name:'Mazda',
+                        year:'2000'
+                    }
+                }"
+
+        >
+            Full info
+        </router-link>
+        <hr>
+        <router-view></router-view>
     </div>
 
 </template>
@@ -17,6 +36,11 @@
         watch:{
             $route(toR,fromR){
                 this.id = toR.params['id']
+            }
+        },
+        methods:{
+            goBackToCars(){
+                this.$router.push('/cars')
             }
         }
     }
---
+++ vue-project/src/pages/CarFull.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h4>Car name: {{$route.query.name}}</h4>
+        <h5>car year: {{year}}</h5>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CarFull",
+        computed:{
+            year(){
+                return this.$route.query.year
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
